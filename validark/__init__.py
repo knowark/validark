d1191788552ab4c7901f09dd29138a2e6c63c6e1
Esteban Echeverry <eecheverry@knowark.com>
2021-04-25 17:56:23 -0500

    Bump version: 0.1.0 → 0.2.0

--- validark/__init__.py
+++ validark/__init__.py
@@ -2,7 +2,7 @@
 
 __author__ = 'Knowark'
 __email__ = 'info@knowark.com'
-__version__ = '0.1.0'
+__version__ = '0.2.0'
 
 from .validate import validate
 from .format import camel_to_snake, snake_to_camel, normalize
